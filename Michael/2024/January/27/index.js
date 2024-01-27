77b82553d2a036be0401779ad0917aecad70b12d
MichaelJHTaggart <mtaggarthawk@gmail.com>
2024-01-27 07:56:55 -0700

    feat(mjht/#6): completed javascript

---
+++ Michael/2024/January/27/index.js
@@ -0,0 +1,7 @@
+// Hackerrank problem
+// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  return a + b;
+}
