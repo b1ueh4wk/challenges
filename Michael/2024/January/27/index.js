70d7cbeeadb17b965caa5d457b8363ce7ede68da
Michael Taggart <mtaggarthawk@gmail.com>
2024-01-27 07:52:35 -0700

    Revert "feat(mjht/#10)"
    This reverts commit bc15b0f831558f086304871f0a559f46020a373a.

--- Michael/2024/January/27/index.js
+++
@@ -1,7 +0,0 @@
-// Hackerrank problem
-// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
-
-int solveMeFirst(int a, int b) {
- // Hint: Type return a+b; below:
-  return a + b;
-}
